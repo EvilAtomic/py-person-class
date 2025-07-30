f6db80ab12a6e0a57a1836f6fbd9505620ac7b6a
artem <artemasarkisan598@gmail.com>
2025-07-30 17:42:43 +0300

    py-Person class

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 class Person:
     people = {}
 
-    def __init__(self,name: str, age: int):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         Person.people[name] = self
 
 def create_person_list(people: list) -> list:
-    for i in people:
-        name = i["name"]
-        age = i["age"]
-        Person(name, age)
-        person = Person.people[name]
-    for i in people:
-        glob = Person.people[i["name"]]
-        if i.get("wife"):
-            glob.wife = Person.people[i["wife"]]
-        if i.get("husband"):
-           glob.husband = Person.people[i["husband"]]
+    Person.people.clear()
+    for item in people:
+        Person(item["name"], item["age"])
+
+    for item in people:
+        person = Person.people[item["name"]]
+        if "wife" in item and item["wife"] is not None:
+            person.wife = Person.people[item["wife"]]
+        if "husband" in item and item["husband"] is not None:
+            person.husband = Person.people[item["husband"]]
+
     return list(Person.people.values())
