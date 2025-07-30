3d5b9056ffc7112fdfc09f5964a3017b370c9949
artem <artemasarkisan598@gmail.com>
2025-07-30 21:57:44 +0300

    py-Person class

--- app/main.py
+++ app/main.py
@@ -6,19 +6,17 @@ class Person:
         self.age = age
         Person.people[name] = self
 
-
-def create_person_list(people: list) -> list[Person]:
+def create_person_list(people: list) -> list:
     Person.people.clear()
     for i in people:
         name = i["name"]
         age = i["age"]
         Person(name, age)
-
+        person = Person.people[name]
     for i in people:
         glob = Person.people[i["name"]]
         if i.get("wife"):
             glob.wife = Person.people[i["wife"]]
         if i.get("husband"):
-            glob.husband = Person.people[i["husband"]]
-
+           glob.husband = Person.people[i["husband"]]
     return list(Person.people.values())
