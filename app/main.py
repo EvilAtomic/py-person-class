640c202e69cac15d333831ce7f4c1081efb422a2
artem <artemasarkisan598@gmail.com>
2025-07-30 15:06:59 +0300

    py-Person class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self,name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people [name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for i in people:
+        name = i["name"]
+        age = i["age"]
+        Person(name, age)
+        person = Person.people[name]
+    for i in people:
+        glob = Person.people[i["name"]]
+        if i.get("wife"):
+            glob.wife = Person.people[i["wife"]]
+        if i.get("husband"):
+           glob.husband = Person.people[i["husband"]]
+    return list(Person.people.values())
