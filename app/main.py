d6b186f2f2977087191ff6e61f680140245d30cb
artem <artemasarkisan598@gmail.com>
2025-07-30 17:33:48 +0300

    py-Person class

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ class Person:
         Person.people[name] = self
 
 def create_person_list(people: list) -> list:
-    Person.people.clear()
-
     for i in people:
         name = i["name"]
         age = i["age"]
