8678ec5446572e6420136a552dbab5decd3c23cd
artem <artemasarkisan598@gmail.com>
2025-07-30 18:24:49 +0300

    py-Person class

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Person:
 def create_person_list(people: list) -> list:
     Person.people.clear()
 
-    
+
     for item in people:
         Person(item["name"], item["age"])
 
@@ -19,4 +19,4 @@ def create_person_list(people: list) -> list:
             person.wife = Person.people[item["wife"]]
         if "husband" in item and item["husband"] is not None:
             person.husband = Person.people[item["husband"]]
-    return list(Person.people.values())
+    return[Person.people[i["name"]] for i in people]
